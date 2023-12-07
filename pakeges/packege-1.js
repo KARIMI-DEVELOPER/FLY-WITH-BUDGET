a3627851bc83c61bfeab9cbddde5376e13e0c7e9
KARIMI-DEVELOPER <150592428+KARIMI-DEVELOPER@users.noreply.github.com>
2023-12-07 11:51:48 +0500

    pack-2 tk kia ha

---
+++ pakeges/packege-1.js
@@ -0,0 +1,20 @@
+// packeges first
+var DayOne = document.querySelector('#day-1');
+var BtnOne = document.querySelector('#btn-1');
+var ListOne = document.querySelector('#list-1');
+var isOpen = false;
+
+BtnOne.addEventListener("click", function () {
+    if (!isOpen) {
+        DayOne.style.height = "50vh";
+        DayOne.style.overflow = "scroll";
+        BtnOne.style.transform = "rotate(45deg)";
+        ListOne.style.display = "block";
+    } else {
+        DayOne.style.height = "20vh";
+        DayOne.style.overflow = "block";
+        BtnOne.style.transform = "rotate(-45deg)";
+        ListOne.style.display = "none";
+    }
+    isOpen = !isOpen; 
+})
