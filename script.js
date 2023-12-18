8213c3cc9ad49cbff011ecd6a94018fb6ac474d4
KARIMI-DEVELOPER <150592428+KARIMI-DEVELOPER@users.noreply.github.com>
2023-12-18 11:29:04 +0500

    emmme

---
+++ script.js
@@ -0,0 +1,16 @@
+// hamburger menu
+var Menu = document.querySelector('#Menu');
+var List = document.querySelector('#listMain');
+var isOpen = false;
+Menu.addEventListener("click",function(){
+    if(!isOpen){
+    List.style.right="0%"
+    List.style.transitionDuration="2s"
+    Menu.style.zIndex="1100"
+    Menu.style.backgroundColor="rgb(81, 65, 40)";
+    }else{
+        List.style.right="-100%"
+    List.style.transitionDuration="2s"
+    }
+    isOpen=!isOpen
+})
