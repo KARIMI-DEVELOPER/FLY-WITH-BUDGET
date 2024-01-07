4accf7ec8d2131a58aded3ffa289f7bfb6d7f698
KARIMI-DEVELOPER <150592428+KARIMI-DEVELOPER@users.noreply.github.com>
2024-01-07 14:24:26 +0500

    fffff

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 var Menu = document.querySelector('#Menu');
 var List = document.querySelector('#listMain');
 var isOpen = false;
-Menu.addEventListener("click",function(){
-    if(!isOpen){
-    List.style.right="0%"
-    List.style.transitionDuration="2s"
-    Menu.style.zIndex="1100"
-    }else{
-        List.style.right="-100%"
-    List.style.transitionDuration="2s"
+Menu.addEventListener("click", function () {
+    if (!isOpen) {
+        List.style.right = "0%"
+        List.style.transitionDuration = "2s"
+        Menu.style.zIndex = "1100"
+    } else {
+        List.style.right = "-100%"
+        List.style.transitionDuration = "2s"
     }
-    isOpen=!isOpen
+    isOpen = !isOpen
 })
+
